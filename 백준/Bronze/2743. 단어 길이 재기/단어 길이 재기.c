9d379cb6c2fb93325f884c95349511bfb4e4b771
Estelle <86831788+kmbalhae07@users.noreply.github.com>
2024-04-01 03:34:21 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[101];
+    scanf("%s", input);
+    
+    int num = strlen(input);
+    printf("%d", num); 
+
+    return 0;
+}
