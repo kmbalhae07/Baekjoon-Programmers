ad850692eb492b72b43fbac8a8b403965f911f71
Estelle <86831788+kmbalhae07@users.noreply.github.com>
2024-04-05 01:48:12 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char *a = "         ,r\'\"7";
+    char *b = "r`-_   ,\'  ,/";
+    char *c = " \\. \". L_r\'";
+    char *d = "   `~\\/";
+    char *e = "      |";
+    char *f = "      |";
+
+    printf("%s\n", a);
+    printf("%s\n", b);
+    printf("%s\n", c);
+    printf("%s\n", d);
+    printf("%s\n", e);
+    printf("%s\n", f);
+
+    return 0;
+}
