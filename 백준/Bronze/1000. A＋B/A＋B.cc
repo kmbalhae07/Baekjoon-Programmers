4b1ec24a27a3f709f677cfa8b9eafb6c4d2b8ded
Estelle <86831788+kmbalhae07@users.noreply.github.com>
2024-05-15 20:43:42 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    //int 10억 이하
+    /*4
+    %d (int)
+    %lld (long long int)**
+    %f (float)
+    %lf (double)**
+    
+    2
+    %c (char)  rasd --- r
+    **%s (char)  rasd --- rasd**/
+    
+    //long long int 10억 이상
+    //============================
+    
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a+b);
+    
+}
